3569a5d288d1c37da853ae827ddabfe1d75eac2f
ishaan-tam <61869234+ishaan-tam@users.noreply.github.com>
2020-08-27 16:31:46 +0530

    Add files via upload

---
+++ login2.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'login2.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(600, 600)
+        font = QtGui.QFont()
+        font.setFamily("Segoe UI")
+        font.setItalic(False)
+        Dialog.setFont(font)
+        Dialog.setFocusPolicy(QtCore.Qt.StrongFocus)
+        Dialog.setSizeGripEnabled(True)
+        self.username = QtWidgets.QLabel(Dialog)
+        self.username.setGeometry(QtCore.QRect(130, 160, 151, 41))
+        font = QtGui.QFont()
+        font.setFamily("Segoe UI")
+        font.setPointSize(16)
+        font.setItalic(False)
+        self.username.setFont(font)
+        self.username.setObjectName("username")
+        self.password = QtWidgets.QLabel(Dialog)
+        self.password.setGeometry(QtCore.QRect(130, 240, 151, 41))
+        font = QtGui.QFont()
+        font.setFamily("Segoe UI")
+        font.setPointSize(16)
+        font.setItalic(False)
+        self.password.setFont(font)
+        self.password.setObjectName("password")
+        self.lineEdit = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit.setGeometry(QtCore.QRect(320, 170, 140, 30))
+        self.lineEdit.setObjectName("lineEdit")
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(380, 430, 90, 41))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2.setGeometry(QtCore.QRect(130, 430, 90, 41))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(220, 10, 151, 51))
+        font = QtGui.QFont()
+        font.setPointSize(26)
+        self.label.setFont(font)
+        self.label.setMouseTracking(False)
+        self.label.setObjectName("label")
+        self.lineEdit_2 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_2.setGeometry(QtCore.QRect(320, 250, 140, 30))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.username.setText(_translate("Dialog", "username"))
+        self.password.setText(_translate("Dialog", "password "))
+        self.pushButton.setText(_translate("Dialog", "login"))
+        self.pushButton_2.setText(_translate("Dialog", "sign up"))
+        self.label.setText(_translate("Dialog", "LOGIN "))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
